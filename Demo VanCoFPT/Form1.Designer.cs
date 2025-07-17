e7dcebe440c32078f79d0c2dd636bf3be9e98e9e
Colek123 <colek.2k@gmail.com>
2025-07-17 14:03:20 +0700

    update mấy cái tào lao

--- Demo VanCoFPT/Form1.Designer.cs
+++ Demo VanCoFPT/Form1.Designer.cs
@@ -31,6 +31,8 @@
             buttonDemo = new Button();
             checkedListBox1 = new CheckedListBox();
             pictureBox1 = new PictureBox();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -59,11 +61,33 @@
             pictureBox1.TabIndex = 4;
             pictureBox1.TabStop = false;
             // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(480, 47);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(82, 19);
+            checkBox1.TabIndex = 5;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(630, 165);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(82, 19);
+            checkBox2.TabIndex = 6;
+            checkBox2.Text = "checkBox2";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
             Controls.Add(pictureBox1);
             Controls.Add(checkedListBox1);
             Controls.Add(buttonDemo);
@@ -71,6 +95,7 @@
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -78,5 +103,7 @@
         private Button buttonDemo;
         private CheckedListBox checkedListBox1;
         private PictureBox pictureBox1;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
     }
 }
